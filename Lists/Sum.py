44e7aeaaa2ea544f165232ec969761deb7716e55
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-09-11 14:32:16 +0530

    [Ariprasath] Add: calculate the sum of all the members in a list

---
+++ Lists/Sum.py
@@ -0,0 +1,76 @@
+'''
+@Author: Ariprasath
+@Date: 2021-09-11 13:52:30
+@Last Modified by: Ariprasath
+@Last Modified time: 2021-09-11 14:28:00
+@Title : calculates the sum of all the members in a list
+'''
+
+
+
+class Error(Exception):
+    def __init__(self,message):
+        super().__init__(self.message)
+class EmptyListError(Error):
+    def __init__(self):
+        self.message="The list is empty"
+        super().__init__(self.message)
+class NonIntError(Error):
+    def __init__(self):
+        self.message="The list contains non-integers"
+        super().__init__(self.message)
+
+def non_int(sample_data):
+    '''
+    Description:
+        checks whether any non-integer value is there in the list passed or not
+    Parameter:
+        sample_data(list): list containing member to be checked
+    Return:
+        boolean value(bool): True if the list contains non-integers and False if the list contains only integers
+    Raises:
+        EmptyListError: Thrown when an empty list is passes
+        NonIntError: Thrown when the list contains non-integers
+    '''
+    for element in sample_data:
+        if type(element)==int:
+            pass
+        else:
+            return True
+            break
+        return False
+
+
+
+def sum_members(sample_data):
+    '''
+    Description:
+        Sums all the members in the list
+    Parameter:
+        sample_data(list): contains integer values
+    Return:
+        sum(int): sum of all the members in the given list
+    Raises:
+        EmptyListError: Thrown when the passed list is empty
+        NonIntError: Thrown when the list contains non-integers
+    '''
+    sum=int()
+    sum=0
+    if len(sample_data)==0:
+        raise EmptyListError
+    elif non_int(sample_data):
+        raise NonIntError
+    else:
+        for element in sample_data:
+            sum+=element
+    return sum
+
+if __name__=="__main__":
+    sample_data=['1',2,3]
+    # sample_data=[]
+    try:
+        print(f"sum-->{sum_members(sample_data)}")
+    except EmptyListError as E:
+        print(E)
+    except NonIntError as I:
+        print(I)
